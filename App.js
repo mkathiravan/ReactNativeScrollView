612fda601eade47747e5fbf25c288cbecbe951d8
Kathiravan <mkathiravanmca@gmail.com>
2021-06-29 12:58:05 +0530

    Update the app.js for scrollView

--- App.js
+++ App.js
@@ -1,21 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react'
+import {SafeAreaView, Text, StyleSheet, View, ScrollView} from 'react-native'
 
-export default function App() {
-  return (
+const App = () => {
+  const [dataSource, setDataSource] = useState([])
+  useEffect(() => {
+    getData()
+  }, [])
+
+  const getData = () => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then((response) => response.json())
+    .then((responseJson) => {
+      setDataSource(responseJson)
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+  }
+
+  const ItemView = (item, index) => {
+    return(
+      <View key={index}>
+
+        <Text style={styles.itemStyle}>
+
+         {item.id}.{item.title}
+
+        </Text>
+
+        <ItemSeparatorView/>
+
+      </View>
+    )
+  }
+
+  const ItemSeparatorView = () => {
+
+    return(
+      <View style={styles.ItemSeparatorStyle} />
+    )
+  }
+
+  return(
+    <SafeAreaView style={{flex: 1}}>
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <ScrollView>
+        {
+          dataSource.map(ItemView)
+        }
+
+      </ScrollView>
+
     </View>
-  );
+
+    </SafeAreaView>
+  )
+
 }
 
+export default App
+
 const styles = StyleSheet.create({
-  container: {
+  container:{
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 40,
+    backgroundColor: 'white'
   },
-});
+  itemStyle : {
+    padding: 10
+  },
+  ItemSeparatorStyle : {
+    height: 0.5,
+    width: '100%',
+    backgroundColor: '#c8c8c8'
+
+  }
+
+})
